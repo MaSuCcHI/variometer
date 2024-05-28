2c2c5b7a66b43866e86e881e00c65e21f414f791
Keisuke <keiske.masuda@gmail.com>
2024-05-29 00:04:40 +0900

    fix

--- main/main.ino
+++ main/main.ino
@@ -1,21 +1,3 @@
-
-/***************************************************************************
-  This is a library for the BMP3XX temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BMP388 Breakout
-  ----> http://www.adafruit.com/products/3966
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
 #include <Adafruit_Sensor.h>
 #include <ArxContainer.h>
 #include <SPI.h>
